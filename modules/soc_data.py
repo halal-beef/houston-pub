3d40bf411b5b2604f71739a0e2a4714cfaf5ba98
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2026-04-19 19:21:22 +0200

    Exynos 9810 Support

--- modules/soc_data.py
+++ modules/soc_data.py
@@ -2,5 +2,10 @@ SOC_DATA = {
     "Exynos9830\00": {
         "rx_address": 0x02022000,
         "usb_struct_offset": 0x0480
+    },
+
+    "Exynos9810\00": {
+        "rx_address": 0x02021800,
+        "usb_struct_offset": 0x0280
     }
 }
