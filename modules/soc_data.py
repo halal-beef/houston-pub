31ef4c50ccec06504581bf3a67282780d136de9c
Umer Uddin <umer.uddin@mentallysanemainliners.org>
2026-08-15 09:40:28 +0100

    soc_data: Import SoC data from VDavid003's tool
    Considering we use the exact same primitive, we may as well support the same devices, other than just 990.
    
    Signed-off-by: Umer Uddin <umer.uddin@mentallysanemainliners.org>

--- modules/soc_data.py
+++ modules/soc_data.py
@@ -4,8 +4,28 @@ SOC_DATA = {
         "usb_struct_offset": 0x0480
     },
 
+    "Exynos3830\00": {
+        "rx_address": 0x02022000,
+        "usb_struct_offset": 0x0480
+    },
+
     "Exynos9810\00": {
         "rx_address": 0x02021800,
         "usb_struct_offset": 0x0280
-    }
+    },
+
+    "Exynos9610\00": {
+        "rx_address": 0x02021800,
+        "usb_struct_offset": 0x0280
+    },
+
+    "Exynos7885\00": {
+        "rx_address": 0x02021800,
+        "usb_struct_offset": 0x0280
+    },
+
+    "Exynos7870\00": {
+        "rx_address": 0x02021800,
+        "usb_struct_offset": 0x0350
+    },
 }
