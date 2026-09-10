7862526b52ab6b5bce33d5b9d30efcfbe3e5e810
Umer Uddin <umer.uddin@mentallysanemainliners.org>
2026-09-10 15:55:24 +0100

    soc_data: Introduce quirks section and re-order SoCs
    Signed-off-by: Umer Uddin <umer.uddin@mentallysanemainliners.org>

--- modules/soc_data.py
+++ modules/soc_data.py
@@ -1,36 +1,47 @@
+# SoC specific quirks
+QUIRK_USB_DROP = 1 << 0
+
+# SoC data
 SOC_DATA = {
     "Exynos9830\00": {
         "rx_address": 0x02022000,
-        "usb_struct_offset": 0x0480
+        "usb_struct_offset": 0x0480,
+        "quirks": 0
     },
 
     "Exynos3830\00": {
         "rx_address": 0x02022000,
-        "usb_struct_offset": 0x0480
+        "usb_struct_offset": 0x0480,
+        "quirks": 0
     },
 
     "Exynos9810\00": {
         "rx_address": 0x02021800,
-        "usb_struct_offset": 0x0280
+        "usb_struct_offset": 0x0280,
+        "quirks": 0
     },
 
     "Exynos9610\00": {
         "rx_address": 0x02021800,
-        "usb_struct_offset": 0x0280
-    },
-
-    "Exynos7885\00": {
-        "rx_address": 0x02021800,
-        "usb_struct_offset": 0x0280
-    },
-
-    "Exynos7870\00": {
-        "rx_address": 0x02021800,
-        "usb_struct_offset": 0x0350
+        "usb_struct_offset": 0x0280,
+        "quirks": 0
     },
 
     "Exynos8890\00": {
         "rx_address": 0x02021800,
-        "usb_struct_offset": 0x0460
+        "usb_struct_offset": 0x0460,
+        "quirks": QUIRK_USB_DROP
+    },
+
+    "Exynos7885\00": {
+        "rx_address": 0x02021800,
+        "usb_struct_offset": 0x0280,
+        "quirks": 0
+    },
+
+    "Exynos7870\00": {
+        "rx_address": 0x02021800,
+        "usb_struct_offset": 0x0350,
+        "quirks": 0
     },
 }
